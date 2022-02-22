9bd11891591c023f7f67a6f11f30e5fa501673a4
tylercmac <tylercmac@gmail.com>
2022-02-21 19:39:37 -0500

    Integrate materialui table

---
+++ app/components/FormatTable.tsx
@@ -0,0 +1,101 @@
+import { useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TablePagination from "@mui/material/TablePagination";
+import moment from "moment";
+
+type Birthday = {
+  id: number;
+  name: string;
+  date: string;
+  stokeLevel: number;
+  daysUntil: number;
+};
+
+export default function FormatTable({ bdays }: { bdays: [Birthday] }) {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const styleRow = (daysUntil: number) => {
+    // if (daysUntil === 0) return { backgroundImage: `url("partyimg.jpg"`};
+    if (daysUntil === 0) return { background: "#00BFFF" };
+    else if (daysUntil === 1) return { background: "#B22222" };
+    else if (daysUntil <= 5) return { background: "#E9967A" };
+    else if (daysUntil <= 10) return { background: "#F0E68C" };
+    else if (daysUntil <= 30) return { background: "#FAFAD2" };
+    else return {};
+  };
+
+  const calcDaysFromToday = (date: string) => {
+    const today = moment().format("YYYY-MM-DD");
+    const years = moment().diff(date, "years");
+    const adjustToday = date.substring(5) === today.substring(5) ? 0 : 1;
+    const nextBirthday = moment(date).add(years + adjustToday, "years");
+    const daysUntilBirthday = nextBirthday.diff(today, "days");
+    return daysUntilBirthday;
+  };
+
+  const addTableProps = bdays.map((bday: Birthday) => {
+    bday.daysUntil = calcDaysFromToday(bday.date);
+    bday.date = moment(bday.date).format("M/D/YY");
+    return bday;
+  });
+
+  const sortByClosest = addTableProps.sort(
+    (a: Object, b: Object) => a.daysUntil - b.daysUntil
+  );
+
+  bdays = sortByClosest;
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell align="right">Bday</TableCell>
+              <TableCell align="right">Days Until</TableCell>
+              <TableCell align="right">Stoke</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {bdays.map((row) => (
+              <TableRow key={row.id} sx={styleRow(row.daysUntil)}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.date}</TableCell>
+                <TableCell align="right">{row.daysUntil}</TableCell>
+                <TableCell align="right">{row.stokeLevel}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={bdays.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </>
+  );
+}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,18 @@
 import { useLoaderData, useActionData, Link } from "remix";
 import type { LoaderFunction } from "remix";
 import { db } from "~/utils/db.server";
-import { getUser } from "~/utils/session.server"
+import { getUser } from "~/utils/session.server";
+import FormatTable from "../components/FormatTable"
 
 export let loader: LoaderFunction = async ({ request }) => {
-  
-  const user = await getUser(request)
-  
+  const user = await getUser(request);
+
   if (!user) return null;
 
   const data = {
     bdays: await db.birthday.findMany({
       where: {
-        userId: user.id
+        userId: user.id,
       },
       take: 20,
       select: { id: true, name: true, date: true, stokeLevel: true },
@@ -25,20 +25,12 @@ export let loader: LoaderFunction = async ({ request }) => {
   return data;
 };
 
-export const action = async ({request} : { request: Request }) => {
+export const action = async ({ request }: { request: Request }) => {
   // Handle server response here when deleting
-}
-
-type Birthday = {
-  id: number;
-  name: string;
-  date: string;
-  stokeLevel: number;
 };
 
 export default function Home() {
   const data = useLoaderData();
-  
 
   return (
     <>
@@ -48,18 +40,21 @@ export default function Home() {
           <button className="btn">Add Birthday!</button>
         </a>
       </div>
-      <ul className="posts-list">
-        {data ? data.bdays.map((bday: Birthday) => (
-          <li key={bday.id}>
-            {bday.name}, {bday.date}, {bday.stokeLevel}
-            <form method="POST" action="/remove">
-              <input type="hidden" name="_method" value="delete" />
-              <input type="hidden" name="id" value={bday.id} />
-              <button className="btn btn-delete">Remove</button>
-            </form>
-          </li>
-        )) : null}
-      </ul>
+      {/* <ul className="posts-list">
+        {data
+          ? data.bdays.map((bday: Birthday) => (
+              <li key={bday.id}>
+                {bday.name}, {bday.date}, {bday.stokeLevel}
+                <form method="POST" action="/remove">
+                  <input type="hidden" name="_method" value="delete" />
+                  <input type="hidden" name="id" value={bday.id} />
+                  <button className="btn btn-delete">Remove</button>
+                </form>
+              </li>
+            ))
+          : null}
+      </ul> */}
+      {data ? <FormatTable bdays={data.bdays} /> : null}
     </>
   );
 }
